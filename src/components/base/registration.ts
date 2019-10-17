de7835f38932994fcf7326279d6a87138a829839
Anatoly <googlsocial97@gmail.com>
2019-10-17 15:14:33 +0700

    Initial commit

---
+++ src/components/base/registration.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+
+import vInputPassword from "@/components/base/v-input-password/v-input-password.vue";
+Vue.component("v-input-password", vInputPassword);
+
+export default {
+  components: {
+    vInputPassword
+  }
+};
---
+++ src/components/base/v-input-password/v-input-password.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="v-title-bar">
+    <input
+      placeholder="7"
+      type="password"
+      v-model="text"
+      @change="test"
+      @input="test"
+    />
+    <div class="mt-2">Value: {{ text }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class vInputPassword extends Vue {
+  @Prop() readonly value!: string;
+
+  text: string = "";
+
+  test() {
+    console.log(this.text);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.v-input-password {
+}
+</style>
